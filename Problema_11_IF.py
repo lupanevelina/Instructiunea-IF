9b732263610e2a59f45c3aa27455fd29003abc75
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-10-17 21:29:27 +0300

    Add files via upload

---
+++ Problema_11_IF.py
@@ -0,0 +1,15 @@
+a=int(input("Introduceti un numar : "))
+b=int(input("Introduceti un numar : "))
+c=int(input("Introduceti un numar : "))
+if (a%2==0) :
+    print(a,"PAR")
+else:
+    print(a,"IMPAR")
+if (b%2==0) :
+    print(b,"PAR")
+else:
+    print(b,"IMPAR")
+if (c%2==0) :
+    print(c,"PAR")
+else:
+    print(c,"IMPAR")        
---
+++ Problema_12_IF.py
@@ -0,0 +1,11 @@
+x=int(input("Introduceti numarul de petale : "))
+if (x%5==4) :
+    print("..un pic ")
+if (x%5==3) :
+    print("..mult ")
+if (x%5==2) :
+    print("..cu pasiune ")
+if (x%5==1) :
+    print("..la nebunie ") 
+if (x%5==0) :
+    print("..deloc ")
