8b80f911b067cdff4c0ff7fcaf2220cc1dbf1de8
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-10-17 22:10:46 +0300

    Add files via upload

---
+++ Problema_15_IF.py
@@ -0,0 +1,11 @@
+x=int(input("Introduceti locul lui Radu : "))
+if x//25==4 :
+    print("A")
+if x//25==3 :
+    print("B")
+if x//25==2 :
+    print("C")
+if x//25==1 :
+    print("D")
+if x//25==0 :
+    print("E")    
